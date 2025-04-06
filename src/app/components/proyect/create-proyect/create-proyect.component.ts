631da006e61cfdd30da7a8883496922c30bb2ea6
JuanCompaire <117914634+JuanCompaire@users.noreply.github.com>
2025-04-06 13:39:47 +0200

    v.4.1.12 create proyect

--- src/app/components/proyect/create-proyect/create-proyect.component.ts
+++ src/app/components/proyect/create-proyect/create-proyect.component.ts
@@ -66,11 +66,13 @@ export class CreateProyectComponent implements OnInit {
     //se le asigna la id del usario que crea el proyecto, a la id del jefe de proyecto
     this.proyect.id_boss = this.user.usuarioId;
     this.proyect.date_created = "";
+    this.selectedUserIds.push(this.user);
     const requestBody = {
       proyectoId: -1,
       name_proyect: this.proyect.name_proyect,
       id_boss: this.proyect.id_boss,
       date_created: this.proyect.date_created,
+      description: this.proyect.description,
       usuarios: this.selectedUserIds.map(user => ({
          usuarioId: user.usuarioId,
          username: user.username,
--- src/app/models/Proyecto.ts
+++ src/app/models/Proyecto.ts
@@ -4,6 +4,7 @@ import { Usuario } from "./Usuario";
 export class Proyecto{
   proyectoId:number = -1 ;
   name_proyect: string = "";
+  description: string = "";
   id_boss: number = 0;
   date_created: string = "";
   usuarios:Usuario[] =[];
